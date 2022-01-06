c0854447f192d7ec49538bab95da9d130850a1fe
Johannes Kochems <jokochems@web.de>
2022-01-06 12:01:54 +0100

    Update correlation analysis
    TODO:
    Resume with clustering!

--- potential_clustering_funcs.py
+++ potential_clustering_funcs.py
@@ -23,7 +23,7 @@ import pandas as pd
 
 
 def create_parameter_combinations(parameters_list, cols):
-    """ Function creates combinations of measures of central tendency,
+    """Function creates combinations of measures of central tendency,
     demand response parameters as well as a given sector.
 
     Parameters
@@ -44,7 +44,7 @@ def create_parameter_combinations(parameters_list, cols):
     """
     combinations_list = []
 
-    # Include all combinations in the list (i.e. measures of central tendencies 
+    # Include all combinations in the list (i.e. measures of central tendencies
     # and parameters)
     col, par = np.meshgrid(cols, parameters_list)
     tuples_list = list(zip(col.flatten(), par.flatten()))
@@ -54,38 +54,49 @@ def create_parameter_combinations(parameters_list, cols):
     return combinations_list
 
 
-def group_potential(df, grouping_cols, mean_cols=[], sum_cols=[], min_cols=[],
-                    max_cols=[], other_cols=[], sector=None, drop=[],
-                    add_cluster=True, weighted_ave=True, weight=[]):
-    """ Function does a grouping using the aggregation functions specified.
+def group_potential(
+    df,
+    grouping_cols,
+    mean_cols=[],
+    sum_cols=[],
+    min_cols=[],
+    max_cols=[],
+    other_cols=[],
+    sector=None,
+    drop=[],
+    add_cluster=True,
+    weighted_ave=True,
+    weight=[],
+):
+    """Function does a grouping using the aggregation functions specified.
     It usually is applied after determining clusters within a cluster analysis.
-    
+
     Parameters
     ----------
     df: pd.DataFrame
         The DataFrame to be grouped
-    
+
     grouping_cols: list
         The column(s) to use for the grouping
-    
+
     mean_cols: list
         Column(s) for which the weigthed average shall be applied
-    
+
     sum_cols: list
         Column(s) for which the sum shall be calculated
-    
+
     sector: str
         The sector (one of "ind", "tcs" and "hoho")
-        
+
     drop: list
         The column(s) to be dropped in grouping
-    
+
     add_cluster: boolean
         If True, add sector and cluster information as index
-        
+
     weighted_ave: boolean
         Determines whether to use a weighted average or a simple one
-    
+
     weight: str
         Column to use for calculating weigthed averages
 
@@ -106,9 +117,13 @@ def group_potential(df, grouping_cols, mean_cols=[], sum_cols=[], min_cols=[],
         if weighted_ave:
             # Calculate a weighted average value using a lambda function
             wm = lambda x: np.average(x, weights=df.loc[x.index, weight])
-            df_grouped_mean = df[mean_cols + grouping_cols].groupby(grouping_cols).aggregate(wm)
+            df_grouped_mean = (
+                df[mean_cols + grouping_cols].groupby(grouping_cols).aggregate(wm)
+            )
         else:
-            df_grouped_mean = df[mean_cols + grouping_cols].groupby(grouping_cols).mean()
+            df_grouped_mean = (
+                df[mean_cols + grouping_cols].groupby(grouping_cols).mean()
+            )
     else:
         df_grouped_mean = df[mean_cols + grouping_cols].groupby(grouping_cols).nth(0)
 
@@ -122,18 +137,27 @@ def group_potential(df, grouping_cols, mean_cols=[], sum_cols=[], min_cols=[],
     # Use nth(0), i.e. first value for the respective columns with only one entry
     df_grouped_other = df[other_cols + grouping_cols].groupby(grouping_cols).nth(0)
 
-    # Combine the different subsets again to obtain a single DataFrame 
+    # Combine the different subsets again to obtain a single DataFrame
     # and set index to include cluster number which will be dropped
-    df_grouped = pd.concat([df_grouped_mean, df_grouped_sum, df_grouped_other,
-                            df_grouped_min, df_grouped_max],
-                           axis=1, join='inner').reset_index()
+    df_grouped = pd.concat(
+        [
+            df_grouped_mean,
+            df_grouped_sum,
+            df_grouped_other,
+            df_grouped_min,
+            df_grouped_max,
+        ],
+        axis=1,
+        join="inner",
+    ).reset_index()
 
     if add_cluster:
-        new_index = sector + '_cluster-' + df_grouped['cluster'].apply(str)
-        df_grouped = df_grouped.set_index(new_index).drop(['cluster'], axis=1)
+        new_index = sector + "_cluster-" + df_grouped["cluster"].apply(str)
+        df_grouped = df_grouped.set_index(new_index).drop(["cluster"], axis=1)
 
     df_grouped = df_grouped.drop(
-        [col for col in df_grouped.columns for el in drop if el in col], axis=1)
+        [col for col in df_grouped.columns for el in drop if el in col], axis=1
+    )
 
     return df_grouped
 
@@ -166,15 +190,19 @@ def map_column_names(availability_time_series, availability_categories):
     -------
 
     """
-    availability_time_series = availability_time_series.rename(columns=availability_categories)
+    availability_time_series = availability_time_series.rename(
+        columns=availability_categories
+    )
     multi_index = pd.MultiIndex.from_tuples(
-        availability_time_series.columns.values).set_names(["Prozess", "sector"])
+        availability_time_series.columns.values
+    ).set_names(["Prozess", "sector"])
     availability_time_series.columns = multi_index
 
     return availability_time_series
 
+
 def determine_missing_cols(potential_T, availability_time_series, sector=None):
-    """ Print info on which columns are missing
+    """Print info on which columns are missing
 
     Parameters
     ----------
@@ -198,8 +226,14 @@ def determine_missing_cols(potential_T, availability_time_series, sector=None):
     print()
 
 
-def assign_availability_remaining(potential_T, availability_time_series, synthetic_cols=[],
-                                  hours_dict={}, days_dict={}, months_dict={}):
+def assign_availability_remaining(
+    potential_T,
+    availability_time_series,
+    synthetic_cols=[],
+    hours_dict={},
+    days_dict={},
+    months_dict={},
+):
     """
     Function assigns availability time series for the remaining categories.
     Remaining categories are the ones for which potential information is given but no
@@ -244,15 +278,17 @@ def assign_availability_remaining(potential_T, availability_time_series, synthet
     # if it is not explicitly
     scalar_dict = {c: 1 for c in diff_cols if c not in synthetic_cols}
     if not len(scalar_dict) == 0:
-        availability_time_series[diff_cols] = pd.DataFrame(scalar_dict,
-                                                           index=availability_time_series.index)
+        availability_time_series[diff_cols] = pd.DataFrame(
+            scalar_dict, index=availability_time_series.index
+        )
     for el in synthetic_cols:
         hours = availability_time_series.index.hour.map(hours_dict[el])
         days = availability_time_series.index.dayofweek.map(days_dict[el])
         months = availability_time_series.index.month.map(months_dict[el])
 
-        availability_time_series[el] = pd.Series(hours * days * months,
-                                                 index=availability_time_series.index)
+        availability_time_series[el] = pd.Series(
+            hours * days * months, index=availability_time_series.index
+        )
 
     return availability_time_series
 
@@ -261,17 +297,17 @@ def assign_availability_remaining(potential_T, availability_time_series, synthet
 # https://stackoverflow.com/questions/17778394/list-highest-correlation-pairs-from-a-large-correlation-matrix-in-pandas
 # accessed 27.08.2020
 def get_redundant_pairs(df):
-    """ Get diagonal and lower triangular pairs of correlation matrix """
+    """Get diagonal and lower triangular pairs of correlation matrix"""
     pairs_to_drop = set()
     cols = df.columns
     for i in range(0, df.shape[1]):
-        for j in range(0, i+1):
+        for j in range(0, i + 1):
             pairs_to_drop.add((cols[i], cols[j]))
     return pairs_to_drop
 
 
 def get_top_abs_correlations(df, n=None, threshold=None):
-    """ Either obtain n largest correlations or largest correlations up to a given threshold """
+    """Obtain n largest correlations or largest correlations up to threshold"""
     au_corr = df.corr().abs().unstack()
     labels_to_drop = get_redundant_pairs(df)
     au_corr = au_corr.drop(labels=labels_to_drop).sort_values(ascending=False)
