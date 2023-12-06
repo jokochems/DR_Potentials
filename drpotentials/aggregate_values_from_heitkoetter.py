bc4c83a1723a023bf8c98bf3b91a1930462be3bd
Johannes Kochems <jokochems@web.de>
2023-12-06 09:33:20 +0100

    Extend value extraction from Heitkoetter 2021

--- drpotentials/aggregate_values_from_heitkoetter.py
+++ drpotentials/aggregate_values_from_heitkoetter.py
@@ -8,16 +8,21 @@ FILE_PATH_OUT = "D:/tubCloud2/Promotion/Literatur/Lastmanagement/full_download_d
 def reaggregate_parameter(root_path: str, year: int):
     """Reaggregate piece of information across all German districts"""
     envelopes_root_path = f"{root_path}/{year}/envelopes/"
+    parameters = pd.DataFrame()
     for path in os.listdir(envelopes_root_path):
         sub_path = os.path.join(envelopes_root_path, path)
         if os.path.isdir(sub_path):
+            cluster_data_set = pd.DataFrame()
             for file in os.listdir(sub_path):
                 deaggregated = pd.read_csv(f"{sub_path}/{file}", index_col=0)
                 deaggregated = deaggregated.sum(axis=1)
+                cluster = file.split("_", 2)[-1].rsplit("_", 2)[0]
+                value = file[:5]
+                cluster_data_set[value] = deaggregated
                 make_directory_if_missing(f"{FILE_PATH_OUT}/{year}")
-                deaggregated.to_csv(
-                    f"{FILE_PATH_OUT}/{year}/{file}", sep=";"
-                )
+                deaggregated.to_csv(f"{FILE_PATH_OUT}/{year}/{file}", sep=";")
+            calculate_metrics(cluster, cluster_data_set, parameters)
+    parameters.to_csv(f"{FILE_PATH_OUT}/{year}/parameters.csv", sep=";")
 
 
 def make_directory_if_missing(folder: str) -> None:
@@ -28,6 +33,36 @@ def make_directory_if_missing(folder: str) -> None:
         pass
     except OSError as e:
         print(f"Failed to create directory: {e}")
+
+
+def calculate_metrics(
+    cluster: str, data_set: pd.DataFrame, parameters: pd.DataFrame
+):
+    """Calculate potential parameters for cluster data set"""
+    parameters.at[cluster, "max_capacity_in_MW"] = (
+        data_set["p_set"].max().item()
+    )
+    parameters.at[cluster, "power_consumption_in_MWh"] = data_set[
+        "p_set"
+    ].sum()
+    parameters.at[cluster, "positive_potential_max_in_MW"] = data_set[
+        "p_max"
+    ].max()
+    parameters.at[cluster, "positive_potential_ave_in_MW"] = data_set[
+        "p_max"
+    ].mean()
+    parameters.at[cluster, "positive_potential_min_in_MW"] = data_set[
+        "p_max"
+    ].min()
+    parameters.at[cluster, "negative_potential_max_in_MW"] = -data_set[
+        "p_min"
+    ].min()
+    parameters.at[cluster, "negative_potential_ave_in_MW"] = -data_set[
+        "p_min"
+    ].mean()
+    parameters.at[cluster, "negative_potential_min_in_MW"] = -data_set[
+        "p_min"
+    ].max()
 
 
 if __name__ == "__main__":
